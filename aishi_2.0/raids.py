c1f1dee68e8abd1d46c5d26373dc2f6cce930d6c
jlam01 <67992204+jlam01@users.noreply.github.com>
2022-04-11 20:00:22 -0400

    Add files via upload

---
+++ aishi_2.0/raids.py
@@ -0,0 +1,29 @@
+def raids ():
+  raids = {'M1 raids':['Tiamat Omega', 'Colossus Omega', 'Leviathan Omega', 'Yggdrasil Omega', 'Luminiera Omega', 'Celeste Omega'],
+  'M2 raids':['Shiva', 'Europa', 'Godsworn Alexiel', 'Grimnir', 'Metatron', 'Avatar'],
+  'T3 raids':['Prometheus', 'Ca Ong', 'Gilgamesh', 'Morrigna', 'Hector', 'Anubis'],
+  'Dragon raids':['Wilnas', 'Wamdus', 'Galleon', 'Ewiyar', 'Lu Woh', 'Fediel', 'Lindwurm'],'T1&2':['Twin Elements', 'Macula Marius', 'Medusa', 'Nezha', 'Apollo', 'Dark Angel Olivia', 'Athena', 'Grani', 'Baal', 'Garuda', 'Odin', 'Lich'],
+  'Disciples raids':['Michael', 'Gabriel', 'Uriel', 'Raphael', 'The Four Primachs'],
+  'Nightmare raids':['Akasha', 'Tiamat Malice', 'Leviathan Malice', 'Phronesis', 'Grand Order'],
+  'ROTB raids':['Huanglong', 'Qilin', 'Huanglong and Qilin'],
+  'Baha raids':['Proto Bahamut','Ultimate Bahamut']}
+  return raids
+
+def gbfroles():
+  roles = ['❤️','🧡','💛','💚','💙','💜','🤎','🖤','🤍']
+  return roles
+
+
+  
+'''
+M1 raids - omega & omegahl
+M2 raids - shiv,alex,grim,met,ava
+T3 raids - prom,co,gil,morig,hector,anubis
+Dragon raids - 6 dragons + wyrm
+T1&2 - 18 man raids te,dao,rose etc
+Disciples raids - all 4 + 4 primach
+Nightmare raids - Tia,Phron,Luc,Levi, akasha, normal go and hl
+ROTB raids - 4 eles + huang qilin
+Baha raids - proto, utlimate, impossible proto, impossible ultimate
+
+'''
